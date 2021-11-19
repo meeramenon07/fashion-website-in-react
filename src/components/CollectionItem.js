35feca4780c013c1d9121aaf1920735275222bf6
Meera Menon <46912602+meeramenon07@users.noreply.github.com>
2021-11-19 16:28:35 +0800

    update CollectionItem

--- src/components/CollectionItem.js
+++ src/components/CollectionItem.js
@@ -1,1 +1,14 @@
 
+import React from "react";
+
+function CollectionItem({ image, name, price }) {
+  return (
+    <div className="collectionItem">
+      <div style={{ backgroundImage: `url(${image})` }}> </div>
+      <h1> {name} </h1>
+      <p> ${price} </p>
+    </div>
+  );
+}
+
+export default CollectionItem;
