51c6e3c0415829f913911ba0dbf8357bd0281475
Meera Menon <46912602+meeramenon07@users.noreply.github.com>
2021-11-19 18:14:29 +0800

    update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import BannerImage from "../assets/pizza.jpeg";
+import BannerImage from "../assets/pexels-godisable-jacob-1943465.jpg ";
 import "../styles/Home.css";
 
 function Home() {
   return (
     <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
       <div className="headerContainer">
-        <h1> Pedro's Pizzeria </h1>
+        <h1> Goed Fashion</h1>
         <p> PIZZA TO FIT ANY TASTE</p>
-        <Link to="/menu">
-          <button> ORDER NOW </button>
+        <Link to="/collection">
+          <button> SHOP NOW </button>
         </Link>
       </div>
     </div>
