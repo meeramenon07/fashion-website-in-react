3531c511b84d1a7a3da799a45b35fb6d15e16423
Meera Menon <46912602+meeramenon07@users.noreply.github.com>
2021-11-21 23:57:03 +0800

    update bags names for bag1 and bag2 in helpers

--- src/helpers/CollectionList.js
+++ src/helpers/CollectionList.js
@@ -1,5 +1,5 @@
-import pexels-artem-beliaikin-1102225.jpg from "../assets/pexels-artem-beliaikin-1102225.jpg";
-import bag2 from "../assets/bag2.jpg";
+import pexels-artem-beliaikin-1102225 from "../assets/pexels-artem-beliaikin-1102225.jpg";
+import pexels-artem-beliaikin-2534961" from "../assets/pexels-artem-beliaikin-2534961.jpg";
 import bag3 from "../assets/bag3.jpg";
 import bag4 from "../assets/bag4.jpg";
 import bag5 from "../assets/bag5.jpg";
@@ -7,13 +7,13 @@ import bag6 from "../assets/bag6.jpg";
 
 export const CollectionList = [
   {
-    name: "bag1",
-    image: bag1,
+    name: "pexels-artem-beliaikin-1102225",
+    image: pexels-artem-beliaikin-1102225
     price: 160.00,
   },
   {
-    name: "bag2",
-    image: bag2,
+    name: "pexels-artem-beliaikin-2534961",
+    image: pexels-artem-beliaikin-2534961,
     price: 500.00,
   },
   {
